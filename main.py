5a466daea2857f5e0203f0296c81f776debf12e9
Krauzy <30305307+Krauzy@users.noreply.github.com>
2021-05-05 20:18:47 -0300

    Add st_canvas

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 from streamlit_drawable_canvas import st_canvas
+
+
+def separator(n=1, side=False):
+    for i in range(n):
+        if not side:
+            st.text(' ')
+        else:
+            st.sidebar.text(' ')
 
 
 # MAIN CONFIG
 st.header('Multilayer Perceptron - Backpropagation')
+separator()
+with st.beta_expander('MNIST'):
+    st.file_uploader(label='Upload Image', type=['PNG', 'JPG', 'JPEG'])
+    st.text('OR')
+    st.markdown('#### Draw the number')
+    canvas = st_canvas(
+        fill_color='',
+        stroke_width=5,
+        stroke_color='#FFD100',
+        background_color='#202020',
+        background_image=None,
+        update_streamlit=True,
+        height=400,
+        drawing_mode='freedraw'
+    )
+    st.button('RUN PREDICTION')
+
+separator(n=2)
+chart = pd.DataFrame(np.random.randn(20, 6), columns=['score', 'a', 'b', 'c', 'd', 'e'])
+st.line_chart(chart['score'])
+st.dataframe(chart)
 
 
 # SIDEBAR CONFIG
@@ -17,20 +48,18 @@ with st.sidebar.beta_expander('STOP MODE'):
         stop_mode_value = side2.text_input(label='Value', value='2000')
     else:
         stop_mode_value = side2.text_input(label='Value', value='0.0001')
-st.sidebar.text(' ')
+separator(side=True)
 
 with st.sidebar.beta_expander('LEARNING RATE'):
     rate = st.slider(label=' ', min_value=0.0000, max_value=1.000, step=0.01, value=0.30)
-st.sidebar.text(' ')
+separator(side=True)
 
 with st.sidebar.beta_expander('OUTPUT FUNCTION'):
     output_func = st.selectbox(label='', options=['Linear', 'Logistics', 'Hyperbolic Tangent'])
-st.sidebar.text(' ')
+separator(side=True)
 
 with st.sidebar.beta_expander('UPLOAD TRAIN'):
     file = st.file_uploader(label='', type=['csv', 'xlsx'])
 st.sidebar.text(' ')
 st.sidebar.text(' ')
 st.sidebar.button(label='CONFIRM')
-
-
